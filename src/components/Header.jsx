98864f80ec8c36223bbb1fb8a8a0b3d1a3b38311
nino <ninosopoian@gmail.com>
2025-04-09 19:31:40 +0400

    feat: -- added img. -- add styles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
 import moduleName from "./header.css";
 
 export default function Header() {
-  return <header className="header">logo & nav </header>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__row">
+          <div className="header__logo">LOGO</div>
+          <div className="header_nav">NAv</div>
+        </div>
+      </div>
+    </header>
+  );
 }
