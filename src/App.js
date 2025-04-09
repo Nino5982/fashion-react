88e93219be0133f1ba4c10239f1b112eaa669672
nino <ninosopoian@gmail.com>
2025-04-09 17:28:47 +0400

    feat: -- add header component, with stils file. -- connected header component  to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-         hi
-      </h1>
+      <Header />
     </div>
   );
 }
 
 export default App;
-
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import moduleName from "./header.css";
+
+export default function Header() {
+  return <header className="header">logo & nav </header>;
+}
