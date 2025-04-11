e075aa257a9c53b2a110eb4b4d0c36b5fd7256e2
nino <ninosopoian@gmail.com>
2025-04-11 18:50:03 +0400

    fix: changed header components folder and names of files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
-import  "./header.css";
+import "./header.css";
+import logoImg from "./../../img/icons/logo.svg";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="container">
         <div className="header__row">
-          <div className="header__logo">LOGO</div>
+          <div className="header__logo">
+            <img src={logoImg} alt="Logo" />
+            <span>Fashion</span>
+          </div>
           <div className="header_nav">NAv</div>
         </div>
       </div>
