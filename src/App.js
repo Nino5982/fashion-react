e10e425469c0fd820f8a5e90bd1422b21ff586ed
nino <ninosopoian@gmail.com>
2025-04-12 16:31:47 +0400

    feat:add section promo with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/header/Header";
+import Promo from "./components/header/promo/Promo";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main></main>
+      <Promo />
     </div>
   );
 }
---
+++ src/components/header/promo/Promo.jsx
@@ -0,0 +1,32 @@
+import "./promo.css";
+import promoImg from "../../../img/images/header-img.jpg";
+
+export default function Promo() {
+  return (
+    <section className="promo">
+      <div className="container">
+        <div className="promo__content">
+          <div className="promo__text">
+            <h1 className="promo__title">
+              <span className="highlight">LET’S</span>
+              EXPLORE
+              <span className="highlight highlight--yellow">UNIQUE</span>
+              CLOTHES.
+            </h1>
+            <p className="promo__desc">
+              Live for Influential and Innovative fashion!
+            </p>
+            <div className="promo__btn--wrapper">
+              <a className="promo__btn" href="#!">
+                Shop Now
+              </a>
+            </div>
+          </div>
+          <div className="promo__img">
+            <img src={promoImg} alt="promo-images" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
